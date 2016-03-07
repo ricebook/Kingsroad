cb049000fcde378a4bade9cd8dbe9db0ff65f022
Carl Chen <ccf.developer@gmail.com>
2016-03-07 17:42:59 +0800

    init project

---
+++ Kingsroad/Kingsroad.h
@@ -0,0 +1,19 @@
+//
+//  Kingsroad.h
+//  Kingsroad
+//
+//  Created by Carl Chen on 3/7/16.
+//  Copyright © 2016 Ricebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kingsroad.
+FOUNDATION_EXPORT double KingsroadVersionNumber;
+
+//! Project version string for Kingsroad.
+FOUNDATION_EXPORT const unsigned char KingsroadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kingsroad/PublicHeader.h>
+
+
